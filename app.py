1dffb1c575f70571431c99af5995e41483cbefb9
= <=>
2019-11-19 09:34:40 +0700

    add like feature

--- app.py
+++ app.py
@@ -2,19 +2,23 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, current_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
 
-#default coder for flask_login
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# default cod   er for flask_login
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view='login'
+login_manager.login_view = 'login'
 app.secret_key = 'abc'
 
-#create database model
+
+# create database model
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -22,12 +26,13 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(10), nullable=False, unique=False)
 
-    #password: 지금 입력한 비밀번호 #self.password: 사용자 비밀번호
+    # password: 지금 입력한 비밀번호 #self.password: 사용자 비밀번호
     def generate_password(self, password):
-        self.password = generate_password_hash(password) #password를 암호화
+        self.password = generate_password_hash(password)  # password를 암호화
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -37,9 +42,11 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, nullable=False)
     created = db.Column(db.DateTime, server_default=db.func.now())
     updated = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    like_time = db.Column(db.Integer, default = 0)
 
     def comments(self):
-        return Comments.query.filter_by(post_id = self.id).all()
+        return Comments.query.filter_by(post_id=self.id).all()
+
 
 class Comments(db.Model):
     __tablename__ = 'comments'
@@ -50,30 +57,34 @@ class Comments(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
-#create table based on the model that I created
+
+# create table based on the model that I created
 db.create_all()
 
-#flask_login > login_manager default code
+
+# flask_login > login_manager default code
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-#set route / link home page
-@app.route('/', methods = ["GET"])
-@login_required 
-def root():   
+
+# set route / link home page
+@app.route('/', methods=["GET"])
+@login_required
+def root():
     # query posts from database
     posts = Post.query.order_by(Post.created.desc()).all()
     # modify our posts so that each post will include all author info:
     for post in posts:
-      post.author = User.query.filter_by(id=post.user_id).first()
+        post.author = User.query.filter_by(id=post.user_id).first()
     return render_template("home.html", posts=posts)
 
-#set route / link creating post
+
+# set route / link creating post
 @app.route('/posts', methods=['POST'])
 @login_required
 def create_post():
-    if request.method=='POST':
+    if request.method == 'POST':
         new_post = Post(body=request.form['body'],
                         user_id=current_user.id)
         db.session.add(new_post)
@@ -90,7 +101,7 @@ def single_post(id):
         flast('Post not found', 'warning')
         return redirect(url_for('root'))
     post.author = User.query.get(post.user_id)
-    if request.method=="POST":
+    if request.method == "POST":
         if post.user_id != current_user.id:
             flash('not allowed to post', 'danger')
             return redirect(url_for('root'))
@@ -103,23 +114,31 @@ def single_post(id):
             db.session.commit()
             return redirect(url_for('single_post', id=id))
         elif action == 'edit':
-            return render_template('single_post.html', post = post, action = action  )
+            return render_template('single_post.html', post=post, action=action)
+        ## like
+        elif action == 'like':
+            post.like_time = post.like_time + 1
+            print(post.like_time,'like_time')
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('root'))
     if not action:
         action = 'view'
-    return render_template('single_post.html', post = post, action= action)
+    return render_template('single_post.html', post=post, action=action)
 
 
-@app.route('/post/<post_id>/comments', methods = ["POST"])
+@app.route('/post/<post_id>/comments', methods=["POST"])
 def create_comment(post_id):
-    new_comment = Comments(body = request.form["body"],
-                          user_id = current_user.id,
-                          post_id = post_id )
-    # comments = Comment.query.get(post.id)
-    db.session.add(new_comment)
+    new_comment = Comments(body=request.form["body"],
+                           user_id=current_user.id,
+                           post_id=post_id)
+    # comments = Commendb.sesst.query.get(post.id)
+    db.sessdb.session.add(new_comment)
     db.session.commit()
-    return redirect(url_for('root')) 
+    return redirect(url_for('root'))
 
-@app.route('/comments/<id>/delete', methods = ["POST"])
+
+@app.route('/comments/<id>/delete', methods=["POST"])
 def delete_comment(id):
     comment = Comments.query.get(id)
     db.session.delete(comment)
@@ -127,48 +146,49 @@ def delete_comment(id):
 
     return redirect(url_for('root'))
 
-#set route / link register page
-@app.route('/register', methods = ["GET", "POST"])
+
+# set route / link register page
+@app.route('/register', methods=["GET", "POST"])
 def register():
-    #logic here
+    # logic here
     if current_user.is_authenticated:
-      return redirect(url_for('root'))
+        return redirect(url_for('root'))
     # If the user submit the form, this line will be checked
     if request.method == 'POST':
         # Use email user provides / check if that email is taken or not
-        check_email = User.query.filter_by(email=request.form['email']).first() 
-        if check_email:  #if email taken
-            flash('Email already taken', 'warning') # we alert the user
-            return redirect(url_for('register')) # then reload the register page again
-        
+        check_email = User.query.filter_by(email=request.form['email']).first()
+        if check_email:  # if email taken
+            flash('Email already taken', 'warning')  # we alert the user
+            return redirect(url_for('register'))  # then reload the register page again
+
         # if email not taken, we add new user to the database
         # set new_user as below:
         # = User class take data as below:
         new_user = User(username=request.form['username'],
                         email=request.form['email'])
-        #raw password will be hashed using the generate_password method
+        # raw password will be hashed using the generate_password method
         new_user.generate_password(request.form['password'])
-        db.session.add(new_user) # then we add new user to our session
-        db.session.commit() # then we commit to our database (kind of like save to db)
-        login_user(new_user) # then we log this user into our system
+        db.session.add(new_user)  # then we add new user to our session
+        db.session.commit()  # then we commit to our database (kind of like save to db)
+        login_user(new_user)  # then we log this user into our system
         flash('Successfully create an account and logged in', 'success')
         # and redirect user to our root which reder to home.html
-        return redirect(url_for('root')) 
+        return redirect(url_for('root'))
 
-    # By default (GET REQUEST), python will skip this condition... 
+        # By default (GET REQUEST), python will skip this condition...
     # ...and just return render_template at the end of this function. 
     return render_template("register.html")
 
 
-#set route / link login page
-@app.route('/login', methods = ["GET", "POST"])
+# set route / link login page
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    #logic here
+    # logic here
     if current_user.is_authenticated:
-      return redirect(url_for('root'))
+        return redirect(url_for('root'))
     if request.method == 'POST':
-        #check if the email that user provides exsist?
-        user = User.query.filter_by(email=request.form['email']).first() 
+        # check if the email that user provides exsist?
+        user = User.query.filter_by(email=request.form['email']).first()
         if not user:
             flash('Email is not registered', 'warning')
             return redirect(url_for('register'))
@@ -180,9 +200,24 @@ def login():
         flash('Wrong password', 'warning')
         return redirect(url_for('login'))
     return render_template("login.html")
-    
 
-#logout
+@app.route('/user/<int:user_id>')
+def Profile(user_id):
+    posts = Post.query.filter(Post.user_id == user_id).all()
+    # modify our posts so that each post will include all author info:
+    for post in posts:
+        post.author = User.query.filter_by(id=post.user_id).first()
+    return render_template("profile.html", posts=posts[::-1])
+
+@app.route('/post/<int:post_id>')
+def Posts(post_id):
+    post = Post.query.filter(Post.id == post_id).first()
+    post.author = User.query.filter_by(id=post.user_id).first()
+    return render_template("post.html", post=post)
+
+
+
+# logout
 @app.route('/logout')
 @login_required
 def logout():
@@ -190,7 +225,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-
-#default code to run server in flask
+# default code to run server in flask
 if __name__ == "__main__":
     app.run(debug=True)
